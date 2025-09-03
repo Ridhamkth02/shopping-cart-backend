3d5682beb31b19b694a04275137f9bce34cc15e4
Ridham Gupta <Ridhamkth@gmail.com>
2025-09-03 23:32:49 +0530

    Initialize project and add core entities: Product, Image, Category

---
+++ src/main/java/com/guptashutters/shoppingcart/model/Product.java
@@ -0,0 +1,34 @@
+package com.guptashutters.shoppingcart.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private int inventory;
+    private String description;
+
+    private Category category;
+
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Image> images;
+
+
+
+}
